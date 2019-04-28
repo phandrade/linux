46fc044edbc95f367bc3fa8b50745bba00f18df6
Paulo Andrade <paulotlj@gmail.com>
2019-04-28 19:00:47 -0300

    exemplos de variaveis reservadas

--- shellscript/variaveis.sh
+++ shellscript/variaveis.sh
@@ -20,4 +20,15 @@ SAIDA_CAT=$(cat /etc/passwd | grep paulo)
 
 echo "$SAIDA_CAT"
 
-# hehehe
+echo "----------------------------------"
+
+echo "Parametro 1: $1"
+echo "Parametro 2: $2"
+echo "Todos os parametros: $*"
+echo "Quantidade de parametros: $#"
+
+echo "Saida do ultimo comando(0 para sucesso e 1 para erro): $?"
+echo "PID: $$"
+echo "Nome do script executado: $0"
+
+#he he he he
