8231a0221512c742770706bdf6db11ffb8a5e5c6
Paulo Andrade <paulotlj@gmail.com>
2019-05-01 17:43:22 -0300

    for com arrays e while

--- shellscript/for.sh
+++ shellscript/for.sh
@@ -12,3 +12,24 @@ for i in $(seq 10)
 do
   echo $i
 done
+
+echo "For 3 (array) ---------"
+Frutas=(
+"Laranja",
+"Ameixa",
+"Abacaxi",
+"Melancia",
+"Jabuticaba"
+)
+for fruta in "${Frutas[@]}"
+do
+  echo "$fruta"
+done
+
+echo "while ---------"
+contador=0
+while [[ $contador -lt ${#Frutas[@]} ]]
+do
+  echo "$contador - ${Frutas[$contador]}"
+  contador=$(($contador+1))
+done
