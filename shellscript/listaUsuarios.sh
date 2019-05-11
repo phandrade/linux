fca5fe0e437024caae77061e1d91460d9287ec46
Paulo Andrade <paulotlj@gmail.com>
2019-05-11 20:42:18 -0300

    atualizazao de versao com novas features, como uso de chave_flag e shift

--- shellscript/listaUsuarios.sh
+++ shellscript/listaUsuarios.sh
@@ -17,9 +17,15 @@
 # Histórico:
 #
 #   v1.0 07/05/2019, Paulo:
-#       - Início do programa com a adição dos parâmetros
+#       - Início do programa com a adição dos parâmetros e uso do IF para tratar
+#         os valores.
 #   v1.1 11/05/2019, Paulo:
 #       - Uso do CASE para tratar os valores do primeiro parâmetro
+#   v1.2 11/05/2019, Paulo:
+#       - Inclusao do parametro para listar os usuarios em maiuscula
+#       - Uso de chave_flag para combinacao dos dois parametros principais
+#       - Uso do while para iterar sobre os parametros de entrada
+#       - Uso do shift para leitura sequencial dos parametros de entrada
 # ------------------------------------------------------------------------ #
 # Testado em:
 #   bash 4.3.48
@@ -38,18 +44,32 @@ $(basename $0) - [OPÇÕES]
 -v - Versão do programa
 -s - Ordenar a saída
 "
-VERSAO="1.1"
+VERSAO="1.2"
+CHAVE_ORDENACAO=0
+CHAVE_MAIUSCULA=0
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- EXECUÇÃO ----------------------------------------- #
-case $1 in
-  -h ) echo "$MENSAGEM_USO"
-    ;;
-  -v ) echo "$VERSAO"
-    ;;
-  -s ) echo "$USUARIOS" | sort
-    ;;
-   * ) echo "$USUARIOS"
-esac
-exit 0
+while [[ -n $1 ]]; do
+  case $1 in
+    -h ) echo "$MENSAGEM_USO" && exit 0
+      ;;
+    -v ) echo "$VERSAO" && exit 0
+      ;;
+    -s ) CHAVE_ORDENACAO=1
+      ;;
+    -m ) CHAVE_MAIUSCULA=1
+      ;;
+     * ) echo "Opção inválida, use o -h para obter ajuda." && exit 1
+  esac
+  shift
+done
+
+if [[ $CHAVE_MAIUSCULA -eq 1 ]]; then
+  USUARIOS=$(echo "$USUARIOS" | tr [a-z] [A-Z])
+fi
+if [[ $CHAVE_ORDENACAO -eq 1 ]]; then
+  USUARIOS=$(echo "$USUARIOS" | sort)
+fi
+echo "$USUARIOS"
 # ------------------------------------------------------------------------ #
